40813bba11a52cbaafc2db87f7c0409540e94b14
khadizaliza <khadizaakter19836@gmail.com>
2022-04-25 02:50:52 +0600

    first commit

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const AddUser = (user) => {
+    const handleAddUser = event =>{
+      event.preventDefault(); 
+      const name = event.target.name.value;
+      const email = event.target.email.value;
+
+      const user = {name, email};
+      fetch('http://localhost:5000/user', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res => res.json ())
+    .then(data =>{
+        console.log('success', data);
+        alert('users added successfully!!!');
+        event.target.reset()
+    })
+    }
+    
+    return ( 
+        <div>
+           <h2>Please add a new User</h2>
+           <form onSubmit={handleAddUser}>
+             <input type="text" name='name' placeholder='name' required/> 
+             <br /> 
+             <input type="email" name='email' placeholder='email' required/>
+             <br />  
+             <input type="submit" value='Add User'/>  
+            </form> 
+        </div>
+    );
+};
+
+export default AddUser;
